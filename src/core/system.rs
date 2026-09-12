666fe911cb5ae621a7a9741cbb3f4fbe8f67349b
icaro-s16 <santanaikaru@gmail.com>
2026-09-12 16:17:05 -0300

    Update .gitignore

--- src/core/system.rs
+++ src/core/system.rs
@@ -1,25 +1,34 @@
-const MEMORY_SIZE: u16 = 8_192;
+const MEMORY_SIZE: u16 = 1024 * 8;
 const DISPLAY_HEIGHT: u16 = 144;
 const DISPLAY_WIDGHT: u16 = 160;
 
 pub struct GbSystem{
-    memory: [u8; MEMORY_SIZE],
+    wram: [u8; MEMORY_SIZE],
+    vram: [u8; MEMORY_SIZE],
     display: [[u8; DISPLAY_WIDGHT]; DISPLAY_HEIGHT],
-    AF: u16,
-    BC: u16,
-    DE: u16,
-    HL: u16,
+    A: u8,
+    F: u8,
+    B: u8,
+    C: u8,
+    D: u8,
+    E: u8,
+    H: u8,
+    L: u8,
     SP: u16,
     PC: u16
 }
 
 struct Opcode{
     mnemonic: String, 
-    raw_val: u8,
+    raw_val: u16,
     operand: Option<u8>
 }
 
 impl Opcode{
-    fn new(pc: u16) -> Self;
+    fn new(pc: u16) -> Self{
+        match(pc){
+
+        }
+    }
     fn exec(self, system: &mut GbSystem);
 }
